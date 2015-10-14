a6be062fc30e6050a328941078257e2573f2589c
fred <fredchen90@gmail.com>
2015-10-14 23:13:23 +0800

    完成可新增新開銷且index可以列出所有紀錄

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,27 @@
 class RecordsController < ApplicationController
+
+	def index
+		@records = Record.all
+	end
+
+	def new
+		@record = Record.new
+	end
+
+	def create
+		@record = Record.new(record_params)
+		if @record.save
+			redirect_to records_path
+		else
+			render :new
+		end
+
+	end
+
+	private
+
+	def record_params
+		params.require(:record).permit(:description, :price)
+	end
+
 end
